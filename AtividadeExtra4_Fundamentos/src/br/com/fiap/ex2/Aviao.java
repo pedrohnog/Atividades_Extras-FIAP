57a34459d2749caea6ffbc6176d8e5321f1e53e3
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-05-31 18:32:29 -0300

    Alteração na classe Aviao

--- AtividadeExtra4_Fundamentos/src/br/com/fiap/ex2/Aviao.java
+++ AtividadeExtra4_Fundamentos/src/br/com/fiap/ex2/Aviao.java
@@ -4,46 +4,50 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Aviao extends Thread {
-	
+
 	private String nomeVoo;
 	private Aeroporto aeroporto;
-	
+
 	private boolean emVoo = false;
-	
+
 	public Aviao(String nomeVoo, Aeroporto aeroporto) {
 		this.nomeVoo = nomeVoo;
 		this.aeroporto = aeroporto;
-		
+	}
+
+	@Override
+	public void run() {
 		this.decolar();
+		this.voar();
+		this.aterrissar();
 	}
-	
-	public void decolar() {
-		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S")) + "] VOO " + this.nomeVoo + " AGUARDANDO PISTA PARA DECOLAGEM");
-		if(this.aeroporto.isPistaDisponivel()) {
-			System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S")) + "] VOO " + this.nomeVoo + " DECOLANDO");
-			this.voar();
-			this.aeroporto.setPistaDisponivel(false);
-		}
+
+	public synchronized void decolar() {
+		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S"))
+				+ "] VOO " + this.nomeVoo + " AGUARDANDO PISTA PARA DECOLAGEM");
+		this.aeroporto.isPistaDisponivel();
+		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S"))
+				+ "] VOO " + this.nomeVoo + " DECOLANDO");
+		this.aeroporto.setPistaDisponivel(false);
 	}
-	
-	public void voar() {
+
+	public synchronized void voar() {
 		try {
 			this.emVoo = true;
 			sleep(2 * 1000l);
-			this.aterrissar();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void aterrissar() {
-		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S")) + "] VOO " + this.nomeVoo + " AGUARDANDO PISTA PARA POUSO");
-		if(this.aeroporto.isPistaDisponivel()) {
-			System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S"))
-					+ "] VOO " + this.nomeVoo + " POUSANDO");
-			this.aeroporto.setPistaDisponivel(false);
-			this.emVoo = false;
-		}
+
+	public synchronized void aterrissar() {
+		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S"))
+				+ "] VOO " + this.nomeVoo + " AGUARDANDO PISTA PARA POUSO");
+		this.aeroporto.isPistaDisponivel();
+		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.S"))
+				+ "] VOO " + this.nomeVoo + " POUSANDO");
+		this.aeroporto.setPistaDisponivel(false);
+		this.emVoo = false;
 	}
 
 	public String getNomeVoo() {
