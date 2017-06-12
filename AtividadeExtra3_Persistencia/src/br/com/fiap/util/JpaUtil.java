475f78ecdaffdf036ca2a478d9dde958bb9169dc
Pedro Nogueira <pedrohnog@users.noreply.github.com>
2017-06-11 23:13:40 -0300

    AtividadeExtra3_Persistencia

---
+++ AtividadeExtra3_Persistencia/src/br/com/fiap/util/JpaUtil.java
@@ -0,0 +1,15 @@
+package br.com.fiap.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaPU");
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+}
